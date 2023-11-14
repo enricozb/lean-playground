008d979310d60b28f3ec1bbe8d0e6f8e1cd42403
Enrico Zandomeni Borba <enricozb@gmail.com>
2023-11-13 22:22:43 -0500

    remove lean.nix

--- flake.nix
+++ flake.nix
@@ -1,37 +1,18 @@
 {
-  description = "lean 4 playground";
+  description = "lean4 playground";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.lean-nix.url = "github:enricozb/lean.nix";
   inputs.lean4.url = "github:leanprover/lean4/v4.3.0-rc1";
-  inputs.lean-mathlib-src = {
-    url = "github:leanprover-community/mathlib4/v4.3.0-rc1";
-    flake = false;
-  };
 
-  outputs = { self, nixpkgs, flake-utils, lean-nix, lean4, lean-mathlib-src }:
+  outputs = { self, nixpkgs, flake-utils, lean4 }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lean4-pkgs = lean4.packages.${system};
-        lean-nix-pkgs = lean-nix.packages.${system};
-
-        lean-mathlib = lean-nix-pkgs.lake2nix {
-          name = "mathlib";
-          src = lean-mathlib-src;
-          lean-toolchain = lean4-pkgs;
-        };
-
       in {
         devShells.default = pkgs.mkShell {
-          packages = [
-            lean4-pkgs.lean
-            lean4-pkgs.vscode
-
-            lean-mathlib.package.modRoot
-          ];
+          packages = [ lean4-pkgs.lean-all lean4-pkgs.vscode ];
         };
       });
-
 }
